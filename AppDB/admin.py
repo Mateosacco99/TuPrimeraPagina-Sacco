715f734ffd745a40a358c1457a923cfb75e15bd1
Mateosacco99 <mateosacco99@gmail.com>
2023-12-05 12:32:01 -0300

    Commit 1.3

--- AppDB/admin.py
+++ AppDB/admin.py
@@ -4,9 +4,7 @@ from AppDB import models
 # Register your models here.
 
 admin.site.register(models.Cliente)
-admin.site.register(models.Vendedor)
-admin.site.register(models.Vehiculo)
-@admin.register(models.VehiculoStock)
+@admin.register(models.Vehiculo)
 class VehiculoAdmin(admin.ModelAdmin):
     list_display = ("modelo_vehiculo", "ano_vehiculo", "precio")
     list_filter = ("modelo_vehiculo", "precio")
--- AppDB/models.py
+++ AppDB/models.py
@@ -7,19 +7,9 @@ class Cliente(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     
-class Vendedor(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.EmailField()
-    
 class Vehiculo(models.Model):
     modelo_vehiculo = models.CharField(max_length=30)
     ano_vehiculo = models.IntegerField()
     precio = models.IntegerField()
     
-class VehiculoStock(models.Model):
-    modelo_vehiculo = models.CharField(max_length=30)
-    ano_vehiculo = models.IntegerField()
-    precio = models.IntegerField()
-    disponible = models.BooleanField()
     
